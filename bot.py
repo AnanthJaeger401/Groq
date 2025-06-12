699530162e4e3e852b91f519872ae3bd5c48926a
AnanthJaeger401 <140995752+AnanthJaeger401@users.noreply.github.com>
2025-06-12 17:41:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ async def main():
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_telegram_message, trigger="cron", hour=19, minute=30, args=[application])
+    scheduler.add_job(send_telegram_message, trigger="cron", hour=14, minute=0, args=[application])
     scheduler.start()
 
     print("Scheduler started. Waiting for jobs...")
